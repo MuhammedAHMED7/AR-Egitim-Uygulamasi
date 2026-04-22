9f104c09b7ee5ad2b1af90e6653b24f5ca2bb8a5
Perihanceliko <pericelikoglu@gmail.com>
2026-04-23 00:16:43 +0300

    Initial commit - VR project

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float mouseSpeed = 100f;
+    float xRotation = 0f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
